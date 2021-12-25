5b5c96bc2a1b7980149733d93c18897a09dfa2a9
JohnWreck <john.wreck@gmail.com>
2021-12-25 23:54:13 +0100

    stuff done

--- setup.sh
+++ setup.sh
@@ -22,13 +22,13 @@ mkdir -p $HOME/.config/dunst
 
 ln -s $HOME/.dotfiles/.bashrc $HOME/.bashrc
 #ln -s $HOME/.dotfiles/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
-ln -s $HOME/.dotfiles/kitty/ $HOME/.config/kitty
+#ln -s $HOME/.dotfiles/kitty $HOME/.config/kitty
 ln -s $HOME/.dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim 
 ln -s $HOME/.dotfiles/i3/config $HOME/.config/i3/config 
 ln -s $HOME/.dotfiles/.i3blocks.conf $HOME/.i3blocks.conf 
 ln -s $HOME/.dotfiles/.scripts $HOME/.scripts
 ln -s $HOME/.dotfiles/.icons $HOME/.icons 
 ln -s $HOME/.dotfiles/.fonts $HOME/.fonts 
-ln -s $HOME/.dotfiles/dunst/ $HOME/.config/dunst
+#ln -s $HOME/.dotfiles/dunst $HOME/.config/dunst
 
 echo "Links done."
