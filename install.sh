af39586e4c513bb61f7b5a6333b7a7147613a5df
JohnWreck <john.wreck@gmail.com>
2021-12-25 23:33:03 +0100

    stuff done

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+apt update
+
 apt install xorg i3 i3blocks rofi lxappearance locate fonts-firacode feh picom kitty numlockx xbacklight gir1.2-gtop-2.0 lm-sensors bc curl firefox-esr materia-gtk-theme xinput network-manager-gnome network-manager-openvpn-gnome libnotify-bin arandr -y
 
 ##Optional
-#apt install mpv cmus nnn neovim htop sxiv flameshot dkms software-properties-common speedtest-cli nmap wireshark bmon nvtop iotop pulsemixer p7zip thunar papirus-icon-theme remmina remmina-plugin-rdp whois syncthing grc ufw calcurse file-roller -y
+#apt install mpv cmus nnn neovim htop sxiv flameshot dkms software-properties-common speedtest-cli nmap wireshark bmon iotop pulsemixer p7zip thunar papirus-icon-theme remmina remmina-plugin-rdp whois syncthing grc ufw calcurse file-roller -y
 
