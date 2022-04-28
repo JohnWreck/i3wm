20aef8c701173ae14ee4b95067a2987f8f49e83c
John Wreck <john.wreck@gmail.com>
2022-04-28 13:04:48 +0200

    stuff done

--- setup.sh
+++ setup.sh
@@ -16,19 +16,19 @@ mv $HOME/.profile $HOME/.profile.bak/ 2>/dev/null
 
 echo "Backup done."
 
-##mkdir $HOME/.config/alacritty
-mkdir -p $HOME/.config/i3
 #mkdir -p $HOME/.config/kitty
+mkdir $HOME/.config/alacritty
+mkdir -p $HOME/.config/i3
 mkdir -p $HOME/.config/nvim
 mkdir -p $HOME/.config/dunst
 mkdir -p $HOME/.config/picom
 
 ##ln -s $HOME/.dotfiles/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
-#ln -s $HOME/.dotfiles/.bashrc $HOME/.bashrc
 #ln -s $HOME/.dotfiles/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
 #ln -s $HOME/.dotfiles/kitty/theme/ $HOME/.config/kitty/theme
 #ln -s $HOME/.dotfiles/.i3blocks.conf $HOME/.i3blocks.conf 
 #ln -s $HOME/.dotfiles/.scripts $HOME/.scripts
+ln -s $HOME/.dotfiles/.bashrc $HOME/.bashrc
 ln -s $HOME/.dotfiles/picom/picom.conf $HOME/.config/picom/picom.conf
 ln -s $HOME/.dotfiles/polybar/ $HOME/.config/polybar
 ln -s $HOME/.dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim 
