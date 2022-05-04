4d56d095249af90b0c5235d72c8f45227b3b1bad
John Wreck <john.wreck@gmail.com>
2022-05-04 16:36:16 +0200

    stuff done

--- setup.sh
+++ setup.sh
@@ -1,44 +1,33 @@
 #!/usr/bin/env bash
 
-##mv $HOME/.config/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml.bak 2>/dev/null 
+#mv $HOME/.config/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml.bak 2>/dev/null 
 #mv $HOME/.i3blocks.conf $HOME/.i3blocks.conf.bak 2>/dev/null 
-#mv $HOME/.config/kitty $HOME/.config/kitty.bak 2>/dev/null 
 #mv $HOME/.scripts/ $HOME/.scripts.bak/ 2>/dev/null 
 mv $HOME/.bashrc $HOME/.bashrc.bak 2>/dev/null 
-mv $HOME/.config/nvim/init.vim $HOME/.config/nvim/init.vim.bak 2>/dev/null 
-mv $HOME/.config/i3/config $HOME/.config/i3/config.bak 2>/dev/null 
 mv $HOME/.config/dunst $HOME/.config/dunst.bak 2>/dev/null 
+mv $HOME/.config/i3/config $HOME/.config/i3/config.bak 2>/dev/null 
+mv $HOME/.config/kitty $HOME/.config/kitty.bak 2>/dev/null 
+mv $HOME/.config/nvim/init.vim $HOME/.config/nvim/init.vim.bak 2>/dev/null 
 mv $HOME/.config/picom $HOME/.config/picom.bak 2>/dev/null 
 mv $HOME/.config/polybar $HOME/.config/polybar.bak 2>/dev/null 
-mv $HOME/.icons/ $HOME/.icons.bak/ 2>/dev/null 
 mv $HOME/.fonts/ $HOME/.fonts.bak/ 2>/dev/null 
+mv $HOME/.icons/ $HOME/.icons.bak/ 2>/dev/null 
 mv $HOME/.profile $HOME/.profile.bak/ 2>/dev/null 
 
 echo "Backup done."
 
-#mkdir -p $HOME/.config/kitty
-mkdir $HOME/.config/alacritty
-mkdir -p $HOME/.config/i3
-mkdir -p $HOME/.config/nvim
-mkdir -p $HOME/.config/dunst
-mkdir -p $HOME/.config/picom
-
-##ln -s $HOME/.dotfiles/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
-#ln -s $HOME/.dotfiles/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
-#ln -s $HOME/.dotfiles/kitty/theme/ $HOME/.config/kitty/theme
 #ln -s $HOME/.dotfiles/.i3blocks.conf $HOME/.i3blocks.conf 
 #ln -s $HOME/.dotfiles/.scripts $HOME/.scripts
+#ln -s $HOME/.dotfiles/alacritty/ $HOME/.config/alacritty
 ln -s $HOME/.dotfiles/.bashrc $HOME/.bashrc
-ln -s $HOME/.dotfiles/picom/picom.conf $HOME/.config/picom/picom.conf
-ln -s $HOME/.dotfiles/polybar/ $HOME/.config/polybar
-ln -s $HOME/.dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim 
-ln -s $HOME/.dotfiles/i3/config $HOME/.config/i3/config 
-ln -s $HOME/.dotfiles/.icons $HOME/.icons 
+ln -s $HOME/.dotfiles/.config/dunst/ $HOME/.config/dunst
+ln -s $HOME/.dotfiles/.config/i3/ $HOME/.config/i3 
+ln -s $HOME/.dotfiles/.config/kitty/ $HOME/.config/kitty
+ln -s $HOME/.dotfiles/.config/nvim/ $HOME/.config/nvim
+ln -s $HOME/.dotfiles/.config/picom/ $HOME/.config/picom
+ln -s $HOME/.dotfiles/.config/polybar/ $HOME/.config/polybar
 ln -s $HOME/.dotfiles/.fonts $HOME/.fonts 
-ln -s $HOME/.dotfiles/dunst/dunstrc $HOME/.config/dunst/dunstrc
+ln -s $HOME/.dotfiles/.icons $HOME/.icons 
 ln -s $HOME/.dotfiles/.profile $HOME/.profile 
 
 echo "Links done."
-
-#Create user dirs
-#xdg-user-dirs-update
