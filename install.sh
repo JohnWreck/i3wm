e89a8e16e4e4b128078f4d243ae8bbe7f3af4add
John Wreck <john.wreck@gmail.com>
2022-09-10 11:42:16 +0200

    stuff done

--- install.sh
+++ install.sh
@@ -2,42 +2,16 @@
 
 package=(
 
-#akmod-nvidia
-#alttab
-#amd64-microcode
-#bluez-tools
-#brightnessctl
-#bsdmainutils
-#cups
-#dkms
-#firefox
-#fonts-firacode
-#i3-gaps
-#network-manager-gnome
-#network-manager-openvpn-gnome
-#pipewire
-#pipewire-alsa
-#pipewire-gstreamer
-#pipewire-pulseaudio
-#pipewire-utils
-#remmina
-#remmina-plugin-rdp
-#rfkill
-#subliminal
-#wavemon
-alacritty
 arandr
-bc
-bmon
 bspwm
 calcurse
 cifs-utils
-cmus
 curl
 fedora-workstation-repositories
 feh
 file-roller
 fira-code-fonts.noarch
+firefox
 flameshot
 fontawesome5-fonts-all.noarch
 git
@@ -49,22 +23,10 @@ google-noto-fonts-common.noarch
 google-noto-sans-cjk-ttc-fonts.noarch
 google-noto-sans-gurmukhi-fonts.noarch
 google-noto-sans-sinhala-vf-fonts.noarch
-gpick
-grc
-htop
-iftop
-iotop
-iperf3
-iptraf-ng
 kitty
 lm_sensors
 lxappearance
-mpv
-ncdu
 neovim
-net-tools
-nethogs
-nmap
 nnn
 numlockx
 p7zip
@@ -74,17 +36,10 @@ playerctl
 polybar
 pop-gtk2-theme
 rofi
-rsync
 simple-mtpfs
-speedtest-cli
 sxhkd
 sxiv
-syncthing
 thunar
-thunderbird
-transmission
-whois
-wireshark
 xinput
 xset
 xsetroot
@@ -93,11 +48,9 @@ zathura-plugins-all
 )
 
 ##RPMFusion setup free/nonfree
-dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+            https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 dnf update
 dnf install ${package[*]}
-
-## Fedora workstation repos, enable Google Chrome
-dnf config-manager --set-enabled google-chrome
 
---
+++ install_extras.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+package=(
+
+##alttab
+##amd64-microcode
+##bsdmainutils
+##fonts-firacode
+#akmod-nvidia
+#bluez-tools
+#brightnessctl
+#cups
+#dkms
+#network-manager-gnome
+#network-manager-openvpn-gnome
+#pipewire
+#pipewire-alsa
+#pipewire-gstreamer
+#pipewire-pulseaudio
+#pipewire-utils
+#remmina
+#remmina-plugin-rdp
+#rfkill
+#wavemon
+alacritty
+bc
+bmon
+cmus
+gpick
+grc
+htop
+iftop
+iotop
+iperf3
+iptraf-ng
+mpv
+ncdu
+net-tools
+nethogs
+nmap
+rsync
+speedtest-cli
+subliminal
+syncthing
+thunderbird
+transmission
+whois
+wireshark
+)
+
+dnf update
+dnf install ${package[*]}
+
+## Fedora workstation repos, enable Google Chrome
+#dnf config-manager --set-enabled google-chrome
+
+## Set Suspendlock service
+cp $HOME/.dotfiles/systemd/system/suspendlock@.service /etc/systemd/system/
+systemctl enable suspendlock@john.service
