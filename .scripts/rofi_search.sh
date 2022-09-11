dc7ba2ac1e8e224f63f4ff9d9af1538b85aa05ff
John Wreck <john.wreck@gmail.com>
2022-09-11 11:08:39 +0200

    stuff done

--- .scripts/rofi_search.sh
+++ .scripts/rofi_search.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env sh
 search_input=$(find ~ -maxdepth 1 -type d | grep -v '/\.'| rofi -dmenu -i -p)
 
-kitty -e nnn -d -e $search_input
 
 
-#if [[ $search_input ]]
-#then
-#    if [[ $(pgrep tmux) ]]
-#    then
-#        tmux new-window nnn -d -e $search_input
-#    else
-#        $TERM -e nnn -d -e $search_input
-#    fi
-#fi
+if [[ $search_input ]]
+then
+    $TERM -e nnn -d -e $search_input
+fi
